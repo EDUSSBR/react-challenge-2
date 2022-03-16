772bce653baedcb8434335745abb60fa57ecf460
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2022-03-16 09:51:17 -0300

    ja havia feito mas tive problema no commit (nao sei por que nao atualizava no repo mas no local estava ok)  entao recriei.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard"
+
+interface ContentProps{
+  movies: {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+ }[]
+ categoria: string
+}
+export function Content({categoria, movies}: ContentProps) {
+  return (
+    <div className="container">
+    <header>
+      <span>Categoria: {categoria}</span>
+    </header>
+    <main>
+     <div className="movies-list">
+     {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+      </div>
+    </main>
+    
+    </div>
+    )
 }
